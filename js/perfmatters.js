88f837ae591806def01f42a657f4a4f8808c5d4b
mvollstaedt <max.vollstaedt@gmx.de>
2017-10-05 14:41:26 +0200

    Optimised CRP

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -11,4 +11,16 @@ function logCRP() {
 
 window.addEventListener("load", function(event) {
   logCRP();
+  enableGA();
 });
+
+// Enabling Google Analytics for Website
+
+function enableGA() {
+  (function(w,g){w['GoogleAnalyticsObject']=g;
+  w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+  // Optional TODO: replace with your Google Analytics profile ID.
+  ga('create', 'UA-XXXX-Y');
+  ga('send', 'pageview');  
+}
